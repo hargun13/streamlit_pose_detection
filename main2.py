38c1ee14ce86bbe9629dbf528b523e61e178d460
hargun13 <chandhokhargun@gmail.com>
2023-12-24 23:38:08 +0530

    its working now, fixing the counter-2

--- main2.py
+++ main2.py
@@ -1,127 +1,3 @@
-# import streamlit as st
-# import cv2
-# import mediapipe as mp
-# import numpy as np
-# import av
-# from streamlit_webrtc import WebRtcMode, webrtc_streamer
-# from sample_utils.turn import get_ice_servers
-
-# mp_drawing = mp.solutions.drawing_utils
-# mp_pose = mp.solutions.pose
-
-# # Curl counter variables for left and right hands
-# left_counter = 0
-# left_stage = None
-# left_prev_stage = None
-
-# right_counter = 0
-# right_stage = None
-# right_prev_stage = None
-
-# def calculate_angle(a, b, c):
-#     a = np.array(a)
-#     b = np.array(b)
-#     c = np.array(c)
-
-#     radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
-#     angle = np.abs(radians * 180.0 / np.pi)
-
-#     if angle > 180.0:
-#         angle = 360 - angle
-
-#     return angle
-
-# def process_frame(frame):
-#     global left_counter, right_counter, left_stage, left_prev_stage, right_stage, right_prev_stage
-
-#     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-#         # Recolor image to RGB
-#         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#         image.flags.writeable = False
-
-#         # Make detection
-#         results = pose.process(image)
-
-#         # Recolor back to BGR
-#         image.flags.writeable = True
-#         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-#         try:
-#             if results.pose_landmarks:
-#                 landmarks = results.pose_landmarks.landmark
-
-#                 # Draw landmarks
-#                 mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-#                                            mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
-#                                            mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2))
-
-#                 # Get coordinates for left hand
-#                 shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-#                             landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-#                 elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
-#                         landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-#                 wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
-#                         landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-
-#                 # Get coordinates for right hand
-#                 shoulder1 = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
-#                              landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-#                 elbow1 = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
-#                           landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-#                 wrist1 = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
-#                           landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
-
-#                 # Calculate angles for left and right hands
-#                 angle_left = calculate_angle(shoulder, elbow, wrist)
-#                 angle_right = calculate_angle(shoulder1, elbow1, wrist1)
-
-#                 # Curl counter logic for left hand
-#                 if angle_left > 160:
-#                     left_stage = "down"
-#                 if (angle_left < 45) and left_stage == 'down' and left_prev_stage != 'up':
-#                     left_stage = "up"
-#                     left_counter += 1
-
-#                 # Curl counter logic for right hand
-#                 if angle_right > 160:
-#                     right_stage = "down"
-#                 if (angle_right < 45) and right_stage == 'down' and right_prev_stage != 'up':
-#                     right_stage = "up"
-#                     right_counter += 1
-
-#                 left_prev_stage = left_stage
-#                 right_prev_stage = right_stage
-#             else:
-#                 # Reset counters if pose is not detected
-#                 left_counter = 0
-#                 right_counter = 0
-
-#         except Exception as e:
-#             print(f"Exception: {e}")
-
-#         return image
-
-# def callback(frame: av.VideoFrame) -> av.VideoFrame:
-#     img = frame.to_ndarray(format="bgr24")
-#     processed_frame = process_frame(img)
-#     return av.VideoFrame.from_ndarray(processed_frame, format="bgr24")
-
-# def main():
-#     st.title("Bicep Curl Counter")
-
-#     webrtc_streamer(
-#         key="bicep-curl-counter",
-#         mode=WebRtcMode.SENDRECV,
-#         rtc_configuration={"iceServers": get_ice_servers()},
-#         video_frame_callback=callback,
-#         media_stream_constraints={"video": True, "audio": False},
-#         async_processing=True,
-#     )
-
-#     st.text(f"Left Reps: {left_counter} | Right Reps: {right_counter}")
-
-# if __name__ == '__main__':
-#     main()
 import streamlit as st
 import cv2
 import mediapipe as mp
@@ -134,9 +10,11 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
 # Curl counter variables for left and right hands
+left_counter = 0
 left_stage = None
 left_prev_stage = None
 
+right_counter = 0
 right_stage = None
 right_prev_stage = None
 
@@ -153,8 +31,8 @@ def calculate_angle(a, b, c):
 
     return angle
 
-def process_frame(frame, left_counter_placeholder, right_counter_placeholder):
-    global left_stage, left_prev_stage, right_stage, right_prev_stage
+def process_frame(frame):
+    global left_counter, right_counter, left_stage, left_prev_stage, right_stage, right_prev_stage
 
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
         # Recolor image to RGB
@@ -171,6 +49,11 @@ def process_frame(frame, left_counter_placeholder, right_counter_placeholder):
         try:
             if results.pose_landmarks:
                 landmarks = results.pose_landmarks.landmark
+
+                # Draw landmarks
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                           mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
+                                           mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2))
 
                 # Get coordinates for left hand
                 shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
@@ -197,47 +80,34 @@ def process_frame(frame, left_counter_placeholder, right_counter_placeholder):
                     left_stage = "down"
                 if (angle_left < 45) and left_stage == 'down' and left_prev_stage != 'up':
                     left_stage = "up"
-                    st.session_state.left_counter += 1
+                    left_counter += 1
 
                 # Curl counter logic for right hand
                 if angle_right > 160:
                     right_stage = "down"
                 if (angle_right < 45) and right_stage == 'down' and right_prev_stage != 'up':
                     right_stage = "up"
-                    st.session_state.right_counter += 1
+                    right_counter += 1
 
                 left_prev_stage = left_stage
                 right_prev_stage = right_stage
             else:
                 # Reset counters if pose is not detected
-                st.session_state.left_counter = 0
-                st.session_state.right_counter = 0
+                left_counter = 0
+                right_counter = 0
 
         except Exception as e:
             print(f"Exception: {e}")
-
-        left_counter_placeholder.text(f"Left Reps: {st.session_state.left_counter}")
-        right_counter_placeholder.text(f"Right Reps: {st.session_state.right_counter}")
 
         return image
 
 def callback(frame: av.VideoFrame) -> av.VideoFrame:
     img = frame.to_ndarray(format="bgr24")
-
-    left_counter_placeholder = st.empty()
-    right_counter_placeholder = st.empty()
-
-    processed_frame = process_frame(img, left_counter_placeholder, right_counter_placeholder)
+    processed_frame = process_frame(img)
     return av.VideoFrame.from_ndarray(processed_frame, format="bgr24")
 
 def main():
     st.title("Bicep Curl Counter")
-
-    if "left_counter" not in st.session_state:
-        st.session_state.left_counter = 0
-
-    if "right_counter" not in st.session_state:
-        st.session_state.right_counter = 0
 
     webrtc_streamer(
         key="bicep-curl-counter",
@@ -247,6 +117,8 @@ def main():
         media_stream_constraints={"video": True, "audio": False},
         async_processing=True,
     )
+
+    st.text(f"Left Reps: {left_counter} | Right Reps: {right_counter}")
 
 if __name__ == '__main__':
     main()
